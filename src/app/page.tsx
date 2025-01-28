a7eff758132548d70ea770c2b8aa5dff4cad573a
strange <strangecode93@gmail.com>
2025-01-28 14:59:15 +0530

    completion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
+import { getPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
+import CreatePost from "@/components/CreatePost";
+import PostCard from "@/components/PostCard";
+import WhoToFollow from "@/components/WhoToFollow";
+import { currentUser } from "@clerk/nextjs/server";
 
+export default async function Home() {
+  const user = await currentUser();
+  const posts = await getPosts();
+  const dbUserId = await getDbUserId();
 
-const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
 
-export default Home
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
+        </div>
+      </div>
+
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </div>
+    </div>
+  );
+}
--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,11 +1,11 @@
-// async function TasksPage() {
-//     const response = await fetch("http://localhost:3000/api/tasks", {
-//       cache: "no-store",
-//     });
-//     const tasks = await response.json();
+async function TasksPage() {
+    const response = await fetch("http://localhost:3000/api/tasks", {
+      cache: "no-store",
+    });
+    const tasks = await response.json();
   
-//     console.log("tasks:", tasks);
+    console.log("tasks:", tasks);
   
-//     return <div>TasksPage</div>;
-//   }
-//   export default TasksPage;
+    return <div>TasksPage</div>;
+  }
+  export default TasksPage;
